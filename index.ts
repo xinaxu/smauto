ae794ea19d53f323d1625a4275782f79e88e716f
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2024-04-03 10:22:24 -0700

    fix

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import VastAI from './vastai.ts'
 import {config} from 'dotenv'
 import SessionManager from './manager.ts'
+import logger from './logger.ts'
 config()
 
 const vastai = new VastAI(process.env.VASTAI_APIKEY!)
 const maxSession = parseInt(process.env.MAX_SESSIONS || '1')
 const manager = new SessionManager(vastai, maxSession)
-await manager.run()
+try {
+  await manager.run()
+} catch(e) {
+  logger.error(e)
+}
